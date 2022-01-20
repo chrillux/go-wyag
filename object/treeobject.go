ea24a374a2f661f9af4074f4fbfa792734f5f9a0
Christian Biamont <christian.biamont@quinyx.com>
2022-01-20 22:36:05 +0100

    more tree object stuff and tests.

--- object/treeobject.go
+++ object/treeobject.go
@@ -22,19 +22,46 @@ type gitTreeLeaf struct {
 }
 
 // treeParseOne a tree is a concatenation of records of the format: [mode] space [path] 0x00 [sha-1]
-func treeParseOne(raw io.Reader, pos int) (*gitTreeLeaf, int) {
+func treeParseOne(raw io.Reader, pos int) (gitTreeLeaf, int) {
 	data, _ := ioutil.ReadAll(raw)
-	space := bytes.IndexByte(data[pos:], 32)
+	space := bytes.IndexByte(data[pos:], 32) + pos
 	mode := string(data[pos:space])
-	null := bytes.IndexByte(data[pos:], 0)
+	null := bytes.IndexByte(data[pos:], 0) + pos
 	path := string(data[space+1 : null])
 	sha := hex.EncodeToString(data[null+1 : null+21])
 
-	return &gitTreeLeaf{
+	return gitTreeLeaf{
 		mode: mode,
 		path: path,
 		sha:  sha,
 	}, pos + null + 21
+}
+
+// treeParse parses all lines in a tree commit into a list of tree objects. This is used to deserialize tree data.
+func treeParse(raw io.Reader) []gitTreeLeaf {
+	data, _ := ioutil.ReadAll(raw)
+	pos := 0
+	max := len(data)
+	treeleafs := []gitTreeLeaf{}
+	treeleaf := gitTreeLeaf{}
+	for i := pos; i < max; i += pos {
+		treeleaf, pos = treeParseOne(bytes.NewReader(data), pos)
+		treeleafs = append(treeleafs, treeleaf)
+	}
+	return treeleafs
+}
+
+func treeSerialize(gtls []gitTreeLeaf) []byte {
+	ret := []byte{}
+	for _, gtl := range gtls {
+		ret = append(ret, []byte(gtl.mode)...)
+		ret = append(ret, byte(32))
+		ret = append(ret, []byte(gtl.path)...)
+		ret = append(ret, byte(0))
+		shabytes, _ := hex.DecodeString(gtl.sha)
+		ret = append(ret, shabytes...)
+	}
+	return ret
 }
 
 func NewTreeObject(repo *git.Repository, data io.Reader) *treeObject {
--- object/treeobject_test.go
+++ object/treeobject_test.go
@@ -7,13 +7,17 @@ import (
 	"testing"
 )
 
-func Test_treeParseOne(t *testing.T) {
+func getData() io.Reader {
 	treehash := "02a32a7bc48d64a5b9f04aa5aeea5d91929865d0"
 	o, err := ReadObject(treehash)
 	if err != nil {
 		log.Fatalf("error reading object: %v", err)
 	}
-	data := o.obj.Serialize()
+	return o.obj.Serialize()
+}
+
+func Test_treeParseOne(t *testing.T) {
+	data := getData()
 	type args struct {
 		raw io.Reader
 		pos int
@@ -21,7 +25,7 @@ func Test_treeParseOne(t *testing.T) {
 	tests := []struct {
 		name  string
 		args  args
-		want  *gitTreeLeaf
+		want  gitTreeLeaf
 		want1 int
 	}{
 		{
@@ -30,7 +34,7 @@ func Test_treeParseOne(t *testing.T) {
 				raw: data,
 				pos: 0,
 			},
-			want: &gitTreeLeaf{
+			want: gitTreeLeaf{
 				mode: "100644",
 				path: "catfile.go",
 				sha:  "ef51f132cfe02e71cda4638f01c190cc91e035cc",
@@ -46,6 +50,77 @@ func Test_treeParseOne(t *testing.T) {
 			}
 			if got1 != tt.want1 {
 				t.Errorf("treeParseOne() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_treeParse(t *testing.T) {
+	type args struct {
+		raw io.Reader
+	}
+	tests := []struct {
+		name string
+		args args
+		want []gitTreeLeaf
+	}{
+		{
+			name: "Read whole tree object",
+			args: args{
+				raw: getData(),
+			},
+			want: []gitTreeLeaf{
+				{
+					mode: "100644",
+					path: "catfile.go",
+					sha:  "ef51f132cfe02e71cda4638f01c190cc91e035cc",
+				},
+				{
+					mode: "100644",
+					path: "hashobject.go",
+					sha:  "87bb585a4dc1438365ff495dbb67493be9662804",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := treeParse(tt.args.raw); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("treeParse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_treeSerialize(t *testing.T) {
+	type args struct {
+		gtls []gitTreeLeaf
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: "Serialize a tree object",
+			args: args{
+				gtls: []gitTreeLeaf{
+					{
+						mode: "100644",
+						path: "foo.sh",
+						sha:  "02a32a7bc48d64a5b9f04aa5aeea5d91929865d0",
+					},
+				},
+			},
+			want: []byte{
+				49, 48, 48, 54, 52, 52, 32, 102, 111, 111, 46, 115, 104, 0, 2, 163, 42, 123, 196, 141, 100, 165, 185, 240, 74, 165, 174, 234, 93, 145, 146, 152, 101, 208,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := treeSerialize(tt.args.gtls); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("treeSerialize() = %v, want %v", got, tt.want)
 			}
 		})
 	}
