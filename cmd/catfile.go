1a3922399c1b6be355817ab337032899c3caf7f9
Christian Biamont <christian.biamont@quinyx.com>
2022-02-04 23:34:32 +0100

    remove some boilerplate

--- cmd/catfile.go
+++ cmd/catfile.go
@@ -11,14 +11,9 @@ import (
 // catfileCmd represents the catfile command
 var catfileCmd = &cobra.Command{
 	Use:   "cat-file",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Args: cobra.RangeArgs(1, 1),
+	Short: "Provide content or type information for repository objects",
+	Long:  "Provide content or type information for repository objects",
+	Args:  cobra.RangeArgs(1, 1),
 	Run: func(cmd *cobra.Command, args []string) {
 		catFile(args[0])
 	},
@@ -28,15 +23,6 @@ var printType bool
 
 func init() {
 	rootCmd.AddCommand(catfileCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// catfileCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	catfileCmd.Flags().BoolVarP(&printType, "object type", "t", false, "Print the object type.")
 }
 
