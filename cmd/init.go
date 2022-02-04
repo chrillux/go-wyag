eb3cf22a08ae965ac35cbaf45d9489bc301be200
Christian Biamont <christian.biamont@quinyx.com>
2022-02-04 23:49:30 +0100

    tidy up init command and remove unused deps.

--- cmd/init.go
+++ cmd/init.go
@@ -14,19 +14,23 @@ var initCmd = &cobra.Command{
 	Long:  `Init a new git repo. If no argument is given, init will be done in current directory.`,
 	Args:  cobra.RangeArgs(0, 1),
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
-			args = append(args, ".")
-		}
-		gr := git.NewRepo()
-		err := gr.Init(args[0], true)
-		if err != nil {
-			fmt.Printf("error running init: %v\n", err)
-			return
-		}
-		fmt.Printf("created repo %s\n", args[0])
+		wyagInit(args)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(initCmd)
+}
+
+func wyagInit(args []string) {
+	if len(args) == 0 {
+		args = append(args, ".")
+	}
+	gr := git.NewRepo()
+	err := gr.Init(args[0], true)
+	if err != nil {
+		fmt.Printf("error running init: %v\n", err)
+		return
+	}
+	fmt.Printf("created repo %s\n", args[0])
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,5 @@ go 1.16
 
 require (
 	github.com/spf13/cobra v1.2.1
-	github.com/spf13/viper v1.9.0
 	gopkg.in/ini.v1 v1.63.2
-	gotest.tools v2.2.0+incompatible
 )
